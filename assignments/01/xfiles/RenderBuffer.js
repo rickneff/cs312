de6ee192c46e546d69c31819e84d55775a11519c
Rick Neff <NeffR@byui.edu>
2014-04-22 22:26:30 -0600

    Sixth version of exemplary solution to Assignment 01.

--- assignments/01/xfiles/RenderBuffer.js
+++ assignments/01/xfiles/RenderBuffer.js
@@ -1,3 +1,24 @@
+// The scanline is one line in the imageData
+// we can calculate the offset for ONE time.
+function ScanLine(imageData, row)
+{
+	this.imageData = imageData;
+	// Note that we still have to do this every row,
+   // but the alternative is to pass in some kind of
+   // offset value we are adding to, which would reveal lots
+   // of internal details.
+   // I choose NOT to break encapsulation for that level of optimization.
+	this.rowOffset = imageData.width * row * 4;
+}
+
+ScanLine.prototype.setPixel = function(x, r, g, b, a)
+{
+	var offset = this.rowOffset + (x * 4);
+	this.imageData.data[offset + 0] = r;
+	this.imageData.data[offset + 1] = g;
+	this.imageData.data[offset + 2] = b;
+	this.imageData.data[offset + 3] = a;
+}
 
 function RenderBuffer(elementID)
 {
@@ -6,6 +27,11 @@ function RenderBuffer(elementID)
    this.width = canvas.width;
    this.height = canvas.height;
    this.imageData = this.context.createImageData(canvas.width, canvas.height);
+}
+
+RenderBuffer.prototype.getScanLine = function(row)
+{
+	return new ScanLine(this.imageData, row);
 }
 
 RenderBuffer.prototype.flush = function()
