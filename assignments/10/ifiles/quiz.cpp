57f6f8fe9823c41830feb3ce06f7c03623662f76
Rick Neff <NeffR@byui.edu>
2014-05-14 13:57:38 -0600

    Added assignment 10's ifiles.

---
+++ assignments/10/ifiles/quiz.cpp
@@ -0,0 +1,6 @@
+float LinearTransform(float x, float C, float D, float E, float F)
+{
+   float A = 0.0f; // TODO: Find A in terms of C, D, E, F
+   float B = 0.0f; // TODO: Find B in terms of C, D, E, F (Not A!)
+   return A * x + B;
+}
